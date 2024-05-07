cd8e1524c893e215cc914bb6da1c95dac2323527
Bernardo Nunes <bmn02@outlook.com>
2024-05-07 10:57:15 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import CategoryList from "./_components/category-list"
 import Header from "./_components/header"
 import Search from "./_components/search"
@@ -9,9 +10,20 @@ const Home = () => {
       <div className="px-5 pt-6">
         <Search />
       </div>
-
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src={"/banner_pizza.png"}
+          alt="até 30% de desconto em pizzas!"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   )
